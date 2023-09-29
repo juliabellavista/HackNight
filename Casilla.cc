10361cda4c9202a3ffab54ea2b2e0ef4db25aa6c
elsiitta03 <elsa.blas@icloud.com>
2023-09-30 01:25:17 +0200

    casilla.cc

---
+++ Casilla.cc
@@ -0,0 +1,36 @@
+#include "Casilla.h"
+
+Casilla::Casilla(int id, bool super) {
+    id_Casilla = id;
+    ocupada = false;
+    id_Ocupant = -1;
+    SuperPower = super;
+}
+
+void Casilla::modifica_ocupada() {
+    ocupada = not ocupada;
+}
+
+void Casilla::modifica_id_Ocupant(int new_id_Ocupant) {
+    id_Ocupant = new_id_Ocupant;
+}
+
+void Casilla::modifica_SuperPower() {
+    SuperPower = not SuperPower;
+}
+
+int Casilla::consultar_id_Casilla() const {
+    return id_Casilla;
+}
+
+bool Casilla::consultar_ocupada() const {
+    return ocupada;
+}
+
+int Casilla::consultar_id_Ocupant() const {
+    return id_Ocupant;
+}
+
+bool Casilla::consultar_SuperPower() const {
+    return SuperPower;
+}
