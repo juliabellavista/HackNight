5d7ad12e7331ea75260c071e7b9a0830ce62f65d
unknown <juliabellavista4@gmail.com>
2023-09-29 23:14:25 +0200

    julia

---
+++ Ficha.h
@@ -0,0 +1,61 @@
+#ifndef Ficha_H
+#define Ficha_H
+
+#include "Ficha.h"
+
+#ifndef NO_DIAGRAM	//No aparece en el diagrama
+#include <map>
+#endif
+
+
+
+class Ficha {
+
+private:
+
+    
+    int id_Ficha;
+    int id_Equipo;
+    struct Pos{
+        int x;
+        int y;
+    };
+    bool colocada;
+    bool selecionada;
+    
+
+public:
+             /*     Constructoras     */
+
+    Ficha();
+
+    	       /*     Modificadoras     */
+
+    void modifica_id_Equipo(int id_Ficha,int new_id_Equipo);
+
+    void modifica_Pos(int id_Ficha,Pos new_Pos);
+
+    void modifica_colocada(int id_Ficha,bool new_colocada);
+
+    void modifica_selecionada(int id_Ficha,bool new_selecionada);
+
+    	       /*     Consultoras     */
+
+    int id_Ficha() const;
+
+    int id_Equipo() const;
+
+    Pos pos() const;
+
+    bool colocada() const;
+
+    bool selecionada() const;
+
+
+private:
+
+    
+};
+#endif
+
+
--- main.cc
+++ main.cc
@@ -1,3 +1,37 @@
-//hola soc la julia
-//hola soc en marcel
-//anem a un mirador porfa superluna
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void main(){
+
+    string arg;
+
+    while(cin >> arg){
+        if(arg =="empezar_partida"){
+
+            //crear tablero
+
+        }else if(arg =="selecionar_ficha"){ //id_ficha selecionada
+            //comprobar que id_ficha existe
+            //calcular posibles posiciones
+            //imprimir posibles posiciones
+
+
+        }else if(arg =="mover_ficha"){//id_ficha a mover,//pos a dnd mover
+            //quitar la ficha de dnd esta
+            //poner ficha nueva posicion
+            //quitar ficha antigua posicion
+            //colocar ficha_antigua nueva posicion
+
+        }else if(arg =="deselecionar_ficha"){//id_ficha
+            //quitar ficha selecionada
+            //quitar posibles posiciones
+
+
+        }else if(arg =="acabar_partida"){
+            //eliminar tablero
+        }
+    }
+
+}
