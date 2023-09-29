4ff319d3eb39c03c38d2bfef57075387a5388b50
unknown <juliabellavista4@gmail.com>
2023-09-30 00:26:39 +0200

    canvi Tablero.h i Casilla.h

---
+++ Tablero.h
@@ -0,0 +1,49 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
+#ifndef NO_DIAGRAM	//No aparece en el diagrama
+#include "Casilla.h"
+#include "Ficha.h"
+#include <vector>
+#include <iostream> 
+using namespace std;
+#endif
+
+class Tablero {
+
+private:
+
+    int id_Tablero;
+    vector<Casilla> tablero;
+    int num_Fichas;
+
+public:
+             /*     Constructoras     */
+
+    Tablero();
+
+    	       /*     Modificadoras     */
+
+    void mover_ficha(Casilla vieja, Casilla nueva, int id_Ficha);
+
+
+    	       /*     Consultoras     */
+    
+    int consultar_id();
+
+    vector<bool> calcular_moviments(int id_Ficha);
+
+    vector<bool> consultar_posFicha(int id_Ficha);
+
+    bool tres_en_ralla(vector<Ficha> fichas_jugador);
+
+
+
+
+private:
+
+    
+};
+#endif
+
+
