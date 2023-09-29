d20ff08160ad5e6a90a85179474edcda3da27a4f
Marcel Masip Bagán <marcelmb@10-192-128-36client.eduroam.upc.edu>
2023-09-29 23:34:41 +0200

    marcel.mb

---
+++ SuperPower.h
@@ -0,0 +1,56 @@
+#ifndef SuperPower_H
+#define SuperPower_H
+
+#include "SuperPower.h"
+
+#ifndef NO_DIAGRAM	//No aparece en el diagrama
+#include <iostream>
+#include <vector>
+using namespace std;
+#endif
+
+
+
+class SuperPower {
+
+private:
+
+    int id_SuperPower;
+
+    struct Pos{
+        int x;
+        int y;
+    };
+
+    vector<bool> posibles_movimientos();
+    
+
+public:
+             /*     Constructoras     */
+
+    SuperPower();
+
+    	       /*     Modificadoras     */
+
+    void modifica_id_SuperPower(int id_SuperPower,int new_id_SuperPower);
+
+    void modifica_Pos(int id_SuperPower,Pos new_Pos);
+
+    void modifica_posibles_movimientos(int id_SuperPower, vector<bool> new_posibles_movimientos());
+
+    	       /*     Consultoras     */
+
+    int id_SuperPower() const;
+
+    Pos pos() const;
+
+    vector<bool> posibles_movimientos() const;
+
+
+private:
+
+    
+};
+#endif
+
+
